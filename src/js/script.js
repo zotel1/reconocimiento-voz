5c2269ed2de8a47bb0b46598b2fbd5768f3613ac
zotel1 <zotelsigel@gmail.com>
2024-08-23 01:17:21 -0300

    mejoramos js

--- src/js/script.js
+++ src/js/script.js
@@ -1,56 +1,48 @@
-// Se seleccionan los elementos del documento html, como la salida y el microfono
+// Seleccionamos los elementos del documento html, como la salida y el microfono
 const salida = document.querySelector('.out');
 const microfono = document.querySelector('.in');
 
 // Creamos el evento click para el microfono
 microfono.addEventListener('click', ejecutar);
 
-// Creamos la funcion ejecutar
-
+// Creamos la función ejecutar
 function ejecutar() {
-    // creamos la constante reconocimientoVoz y le asignamos la API webkitSpeechRecognition
-    const reconocimientoVoz = new webkitSpeechRecognition();
-
-    // creamos la constante reconocimiento y le asignamos la constante reconocimientoVoz
-    const reconocimiento = reconocimientoVoz();
+    // Creamos la constante reconocimiento y le asignamos la API webkitSpeechRecognition
+    const reconocimiento = new webkitSpeechRecognition();
 
     // Comenzamos el reconocimiento
     reconocimiento.start();
 
-    // Creamos la funcion reconocimiento que se ejecuta cuando se inicia el reconocimiento
-    reconocimiento.onstart = function() {
+    // Función que se ejecuta cuando se inicia el reconocimiento
+    reconocimiento.onstart = function () {
         // Asignamos la clase mostrar al elemento salida
         salida.classList.add('mostrar');
         // Asignamos el texto a mostrar en el elemento salida
         salida.textContent = 'Escuchando...';
     };
 
-    // Creamos la funcion reconocimiento que se ejecuta cuando se deja de grabar
-    reconocimiento.onspeechend = function() {
+    // Función que se ejecuta cuando se deja de grabar
+    reconocimiento.onspeechend = function () {
         // Asignamos el texto a mostrar en el elemento salida
-        salida.textContent = 'Se dejo de grabar';
+        salida.textContent = 'Se dejó de grabar';
         // Detenemos el reconocimiento
         reconocimiento.stop();
     };
 
-    // Creamos la funcion reconocimiento que se ejecuta cuando se deja de grabar
-    reconocimiento.onresult = function(e) {
+    // Función que se ejecuta cuando se obtienen resultados
+    reconocimiento.onresult = function (e) {
         console.log(e.results[0][0]);
 
         // Destructuramos el objeto e.results[0][0] y obtenemos el transcript y el confidence
-        const{ transcript, confidence } = e.results[0][0];
+        const { transcript, confidence } = e.results[0][0];
 
         // Creamos un elemento p y le asignamos el texto a mostrar
         const texto = document.createElement('p');
-
-        // Asignamos el texto a mostrar en el elemento p
         texto.innerHTML = `Grabado: ${transcript}`;
 
         // Creamos otro elemento p y le asignamos el texto a mostrar
         const fiabilidad = document.createElement('p');
-
-        // Asignamos el texto a mostrar en el elemento p
-        fiabilidad.innerHTML = `Fiabilidad del ${parseInt (confidence*100)}%`
+        fiabilidad.innerHTML = `Fiabilidad del ${parseInt(confidence * 100)}%`;
 
         // Agregamos los elementos p al elemento salida
         salida.appendChild(texto);
